c0fdb440b1bf10356b4b04ac536c8dd9c303993d
ThuyavanKannan <44844268+tk-69@users.noreply.github.com>
2022-11-30 13:09:00 -0500

    Update app.py
    removed keys

--- app.py
+++ app.py
@@ -18,19 +18,19 @@ Session(app)
 #email
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'g.ajaygg@gmail.com'
-app.config['MAIL_PASSWORD'] = 'oiocqtghvwjkiprf'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 #duffel api
-access_token = 'duffel_test_kJLcDXpH11bKR78Y5Kxu7MFMck1D1Xlx0cHWwAgxmb8'
+access_token = ''
 client = Duffel(access_token = access_token)
 
 
 #fliapi
-fliapi = "https://airlabs.co/api/v9/cities?name=Singapore&api_key=961e45ec-e4c1-4ce8-99ef-c9411dde97e2"
+fliapi = "https://airlabs.co/api/v9/cities?name=Singapore&api_key="
 data = requests.get(fliapi).json()
 data = data['response']
 # Initialize Firestore DB
